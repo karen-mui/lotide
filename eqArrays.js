582c79c6a2ffe7ba21f2734bdb4d7073e5eed1ad
Karen Mui <karen_mui@hotmail.com>
2022-11-16 22:37:52 +0000

    function that checks if arrays are identical

---
+++ eqArrays.js
@@ -0,0 +1,21 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+function eqArrays(array1, array2) {
+  let num = 0;
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] === array2[i]) {  
+    num++;
+    }
+  }
+  if (num === array1.length && num === array2.length) {
+    return true;
+  }      
+}
+
+assertEqual(eqArrays(["2", 5, "10"], ["2", 5, "10"]), true); 
