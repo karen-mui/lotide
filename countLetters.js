8066c803a4ae9226d699c0a955cb2fa9722dd91b
Karen Mui <karen_mui@hotmail.com>
2022-11-18 01:51:57 +0000

    count how many letters in string and returns object

---
+++ countLetters.js
@@ -0,0 +1,23 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const countLetters = function(stringInput) {
+  const results = {};
+  for (const letter of stringInput) {
+    if (results[letter]) {
+      results[letter] += 1;
+    } else {
+      results[letter] = 1;
+    }
+  }
+  return results;  
+}
+
+const result1 = countLetters('lighthouse');
+
+assertEqual(result1["h"], 2);
