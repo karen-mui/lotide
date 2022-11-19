044f7539d1e25a085e9373dee1f7cce7b57a85d1
Karen Mui <karen_mui@hotmail.com>
2022-11-19 16:21:08 +0000

    a function for the arr.map() method

---
+++ map.js
@@ -0,0 +1,33 @@
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const eqArrays = function(array1, array2) {
+  let num = 0;
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] === array2[i]) {  
+    num++;
+    }
+  }
+  if (num === array1.length && num === array2.length) {
+    return true;
+  }      
+}
+
+// const words = ["", "control", "to", "major", "tom"];
+
+const map = function(array, callback) {
+  const results = [];
+  for (let item of array) {
+    results.push(callback(item));
+  }
+  return results;
+}
+
+// const results1 = map(words, word => word[0]);
+// console.log(results1);
+
