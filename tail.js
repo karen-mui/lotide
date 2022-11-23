93874fa8f251a8ef70781d5a4f6153cf5d18e009
Karen Mui <karen_mui@hotmail.com>
2022-11-23 02:20:02 +0000

    refactoring wet code to dry; commit tail

--- tail.js
+++ tail.js
@@ -1,19 +1,8 @@
-// FUNCTION IMPLEMENTATION
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
-  }
-  };
+const assertEqual = require('./assertEqual');
   
 function tail(array) {
   let newArray = array.slice(1);
   return newArray;
 }
 
-// Test Case 1: Check the returned array elements
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2); // ensure we get back two elements
-assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs"); // ensure second element is "Labs"
+module.exports = tail;
