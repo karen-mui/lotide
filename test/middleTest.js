ba2a2112a048b21047474a7b58e12b7babd29527
Karen Mui <karen_mui@hotmail.com>
2022-11-23 04:44:13 +0000

    refactored middleTest and tailTest with mocha and chai

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,3 +1,20 @@
+const assert = require('chai').assert;
+
 const middle = require('../middle');
 
-console.log(middle([5, 1, 1]))
+
+describe("#tail", () => {
+  
+  it("returns [2, 3] for [1, 2, 3, 4]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4]), [2, 3]);
+  });
+
+  it("returns [5] for [1, 2, 5, 3, 4]", () => {
+    assert.deepEqual(middle([1, 2, 5, 3, 4]), [5]);
+  });
+
+  it("returns ['Lighthouse'] for ['Hello', 'Lighthouse', 'Labs']", () => {
+    assert.deepEqual(middle(['Hello', 'Lighthouse', 'Labs']), ['Lighthouse']);
+  });
+
+});
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,7 +1,16 @@
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
+
+// const assertEqual = require('../assertEqual');
 const tail = require('../tail');
 
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2); // ensure we get back two elements
-assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs"); // ensure second element is "Labs"
+describe("#tail", () => {
+  
+  it("returns [2, 3] for [1, 2, 3]", () => {
+    assert.deepEqual(tail([1, 2, 3]), [2, 3]);
+  });
+
+  it("returns ['Lighthouse', 'Labs'] for ['Hello', 'Lighthouse', 'Labs']", () => {
+    assert.deepEqual(tail(['Hello', 'Lighthouse', 'Labs']), ['Lighthouse', 'Labs']); 
+  });
+
+});
